e9f02dd5a934ef0132a5a6344cfea017c5af3ab1
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-11-07 15:44:39 -0300

    feature: add tests to the presenter

--- app/src/main/java/com/arthurgonzaga/dictionary/presenter/MainPresenter.kt
+++ app/src/main/java/com/arthurgonzaga/dictionary/presenter/MainPresenter.kt
@@ -3,7 +3,10 @@ package com.arthurgonzaga.dictionary.presenter
 import android.util.Log
 import com.arthurgonzaga.dictionary.api.ApiService
 import com.arthurgonzaga.dictionary.contract.MainContract
+import com.arthurgonzaga.dictionary.model.WordResponse
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.SingleObserver
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainPresenter(
@@ -16,8 +19,16 @@ class MainPresenter(
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { list -> view.setDescription(list[0].origin) },
-                { view.showErrorSnackBar(it.message) }
+                object: SingleObserver<List<WordResponse>>{
+
+                    override fun onSubscribe(d: Disposable) {}
+
+                    override fun onSuccess(list: List<WordResponse>) {
+                        view.setDescription(list[0].origin)
+                    }
+
+                    override fun onError(e: Throwable) { view.showErrorSnackBar(e.message) }
+                }
             )
     }
 
---
+++ app/src/test/java/com/arthurgonzaga/dictionary/presenter/MainPresenterTest.kt
@@ -0,0 +1,67 @@
+package com.arthurgonzaga.dictionary.presenter
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.arthurgonzaga.dictionary.api.ApiService
+import com.arthurgonzaga.dictionary.contract.MainContract
+import com.arthurgonzaga.dictionary.model.WordResponse
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.*
+import java.lang.Exception
+import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
+import io.reactivex.rxjava3.core.Scheduler
+import java.util.concurrent.Callable
+
+
+class MainPresenterTest {
+
+    private var view = mock(MainContract.View::class.java)
+    private var service = mock(ApiService::class.java)
+    private lateinit var presenter: MainPresenter
+
+    @Before
+    fun setUp() {
+        reset(view)
+        reset(service)
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+        presenter = MainPresenter(view, service)
+    }
+
+    @Test
+    fun `should set the description when search a word`() {
+        // Given
+        val word = "cool"
+        val origin = "this is the origin of the word cool"
+        val response = Single.just(listOf(WordResponse(origin)))
+
+        // When
+        `when`(service.searchWord(word)).thenReturn(response)
+        presenter.searchWord(word)
+
+        // Then
+        verify(view, times(1)).setDescription(origin)
+    }
+
+    @Test
+    fun `should show a toast when an error occurs`(){
+        // Given
+        val word = "gdftdgvjghik"
+        val exception = Exception("404 Not found")
+        val response = Single.error<List<WordResponse>>(exception)
+
+        // When
+        `when`(service.searchWord(word)).thenReturn(response)
+        presenter.searchWord(word)
+
+        // Then
+        verify(view, times(1)).showErrorSnackBar(exception.message)
+    }
+
+
+}
