5e4776dc134b807b7efba27cf9e1e1d62f6ecb2c
Arthur Gonzaga <arthurgonzagaxyz@gmail.com>
2021-12-04 09:13:24 -0300

    revome mvp architecture

--- app/src/main/java/com/arthurgonzaga/dictionary/ui/MainActivity.kt
+++ app/src/main/java/com/arthurgonzaga/dictionary/ui/MainActivity.kt
@@ -2,21 +2,13 @@ package com.arthurgonzaga.dictionary.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.widget.Toast
 import com.arthurgonzaga.dictionary.R
-import com.arthurgonzaga.dictionary.contract.MainContract
 import com.arthurgonzaga.dictionary.databinding.ActivityMainBinding
-import com.arthurgonzaga.dictionary.presenter.MainPresenter
 import com.google.android.material.snackbar.Snackbar
 import com.jakewharton.rxbinding4.widget.textChanges
 import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity(), MainContract.View {
-
-    private var _presenter: MainContract.Presenter? = null
-    private val presenter get() = _presenter!!
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -24,8 +16,6 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        _presenter = MainPresenter(this)
 
         setupEditTextListener()
     }
@@ -36,22 +26,19 @@ class MainActivity : AppCompatActivity(), MainContract.View {
             .doOnNext { binding.descriptionTextView.text = "" }
             .debounce(DEBOUNCE_TIMEOUT, TimeUnit.MILLISECONDS)
             .filter { it.toString().isNotBlank() }
-            .doOnNext { text-> presenter.searchWord(text.toString()) }
+            .doOnNext { text ->
+
+            }
             .subscribe()
     }
 
-    override fun setDescription(text: String) {
+    fun setDescription(text: String) {
         binding.descriptionTextView.text = text
     }
 
-    override fun showErrorSnackBar(message: String?) {
+    fun showErrorSnackBar(message: String?) {
         val textId = if(message?.contains("404") == true) R.string.word_not_found else R.string.something_wrong
         Snackbar.make(binding.root, textId, Snackbar.LENGTH_SHORT).show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _presenter = null
     }
 
     companion object {
